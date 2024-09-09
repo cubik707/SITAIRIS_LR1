b47a292bd1614b607b077c153434c18f23e73ead
WiZaRd <wduck17@gmail.com>
2024-09-09 13:33:47 +0300

    Reapply "lol1123456"
    This reverts commit 78be6b4c80052f3813451e230d187687aa222d3e.

--- src/com/vlada/trash/Main.java
+++ src/com/vlada/trash/Main.java
@@ -12,5 +12,7 @@ public class Main {
         System.out.println("well...");
         System.out.printf("someone");
         System.out.println("like");
+        System.out.println("....");
+        System.out.println("YOUUUU");
     }
 }
